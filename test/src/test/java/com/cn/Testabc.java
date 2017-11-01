e4807701262555d80d47b0742f77115653c8a388
wutao2000 <erik20130312@163.com>
2017-11-01 14:09:54 +0800

    test

---
+++ test/src/test/java/com/cn/Testabc.java
@@ -0,0 +1,14 @@
+package com.cn;
+
+import org.testng.annotations.Test;
+
+
+public class Testabc {
+	@Test
+	public void Testabc(){
+      int a ;
+      int b ;
+      System.out.println("abc");
+	}
+      
+}
